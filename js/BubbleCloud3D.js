f2b87b5af7b8e6b5edbea00ea1ed3abeb611dd10
pegasuspect <osman.sekerlen@ozu.edu.tr>
2015-07-08 15:57:54 +0300

    Firefox support added.

--- js/BubbleCloud3D.js
+++ js/BubbleCloud3D.js
@@ -187,6 +187,8 @@ function Bubble(vector, content, settings){
 	this.element = jQuery("<div>").addClass("bubble noTextSelect").html(this.content)
 		.css({
 			"-webkit-transform" : pos,
+			"-ms-transform": pos, //IE 9
+			"transform": pos,
 			top: (settings.frameHeight - size)/2,
 			left: (settings.frameWidth - size)/2,
 			width: size,
@@ -211,7 +213,10 @@ Bubble.prototype.updatePosition = function(){
 	var pos = "translateX("+(this.vector.x)+"px) " + 
 		"translateY("+(this.vector.y)+"px) " + 
 		"translateZ("+(this.vector.z*3)+"px) ";
-	this.element.css("-webkit-transform", pos);
+	this.element
+		.css("-ms-transform", pos) //IE 9
+		.css("-webkit-transform", pos) // Safari
+		.css("transform", pos);
 }
 
 Bubble.prototype.giveAngularRotationOnAxis = function(x, y){
